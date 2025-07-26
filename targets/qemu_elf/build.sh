a4544a7053b3bd7fa5b4b2b9de1b2ef163b5c353
Sebasteuo <sebasjosue84@estudiantec.cr>
2025-07-26 17:53:05 -0600

    qemu_elf build: cd into target dir before compiling

--- targets/qemu_elf/build.sh
+++ targets/qemu_elf/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eux
+cd "$(dirname "$0")"        #  mueve el cwd a targets/qemu_elf
 mkdir -p build
 # Compilamos en 32 bits, estático, con afl-clang-fast
 afl-clang-fast -static -O2 src/mini_png.c -o build/mini_png
