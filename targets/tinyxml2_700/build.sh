d0b903715410642bad199923b57e02bd3a5dfebe
Sebasteuo <sebasjosue84@estudiantec.cr>
2025-08-06 19:01:48 -0600

    feat: add tinyxml2 7.0.0 vulnerable target with PoC

--- targets/tinyxml2_700/build.sh
+++ targets/tinyxml2_700/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-CC=${CC:-afl-clang-fast++}          # tinyxml2 es C++
+CC=${CC:-afl-clang-fast++}
 CXX=${CC}
 CFLAGS="-g -O2 -fsanitize=address -fno-omit-frame-pointer"
 LDFLAGS="-fsanitize=address"
@@ -9,6 +9,5 @@ SRC_DIR="$(dirname "$0")/src"
 BUILD_DIR="$(dirname "$0")/build"
 mkdir -p "$BUILD_DIR"
 
-# Compila la librería y el binario de prueba 'xmltest'
 cd "$SRC_DIR"
 ${CXX} ${CFLAGS} tinyxml2.cpp xmltest.cpp -o "$BUILD_DIR/xmltest" ${LDFLAGS}
